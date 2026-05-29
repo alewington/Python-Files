e58a1c82e1acba985deedf431818ebb99f249e30
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-05-29 13:43:14 +0100

    New File

---
+++ systemVer.py
@@ -0,0 +1,10 @@
+# python version being run is shown in the output
+import sys
+
+print(sys.version)
+"""
+    This code snippet imports the sys module and prints the version.
+    Args:        None
+    Returns:     None
+    Raises:      None
+"""

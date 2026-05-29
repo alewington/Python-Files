5f3644fb0a611cdbd0b784a389e910a9b5ffecb6
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-05-29 14:22:45 +0100

    New File

---
+++ 01-varAndConst/01-VarNaming.py
@@ -0,0 +1,30 @@
+# Variable is a container for storing data values.
+# Its starts lowercase letter and can be a combination of letters and numbers.
+# It cannot start with a number.
+
+x = 1
+steps_taken = 1000
+speedOfCar = 88
+first_Name = "Feida"
+last_Name = "Zhang"
+lightsOn = True
+list_Of_Scores = [90, 80, 70, 60]
+
+print(x)
+print(steps_taken)
+print(speedOfCar)
+print(first_Name)
+print(last_Name)
+print(lightsOn)
+print(list_Of_Scores)
+# You can display them in these fashion.
+# Chose A style and keep with it!
+
+# e.g. snake_case, camelCase, PascalCase, etc.
+snake_case = "This is snake case"
+camelCase = "This is camel case"
+PascalCase = "This is Pascal case"
+
+print(snake_case)
+print(camelCase)
+print(PascalCase)

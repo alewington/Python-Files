59c9a29934cc4f86aad972eb046db62b8663c83f
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-05-30 13:08:15 +0100

    new File: data input

---
+++ 01-varAndConst/06-userInput.py
@@ -0,0 +1,19 @@
+# User Input
+
+# input() function is used to take input from the user.
+# It always returns a string.
+# name = input("Enter your name: ")
+# print("Hello, " + name + "!")
+
+name = input("Enter your name: ")
+print(name)
+
+# To take numerical input, we need to convert the
+# string to the desired data type using int()
+# or float() functions.
+# age = int(input("Enter your age: "))
+# print("You are " + str(age) + " years old.")
+age = int(input("Enter your age: "))
+print(age)
+
+# Did you notice the casting of int? line 16.

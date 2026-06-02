dc02feaf8f60516ceb2a6fe520110e2e0758bb6d
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-02 09:12:59 +0100

    update

--- 02-dataTypes/01-numbers_Int/01-integer.py
+++ 02-dataTypes/01-numbers_Int/01-integer.py
@@ -1,7 +1,8 @@
 # integer
 
 # integer refer to whole numbers like 1, 5, 10 , 1,000, -2, 1,000,000,000
-# As long as you have enough memory you can store as big and as many integers to fill it up in Python 3.
+# As long as you have enough memory you can store as big and as many integers
+# to fill it up in Python 3.
 
 # Assigning integers
 numInt = 100
@@ -11,12 +12,13 @@ print(type(numInt))  # prints <class 'int'>
 
 
 # casting to int
-numInt2 = int(1.59)  #float to int
+numInt2 = int(1.59)  # float to int
 print(numInt2)  # prints 1
 print(type(numInt2))  # prints <class 'int'>
 
 
-# please note this is NOT rounding, it just removes the decimal part and keeps the whole number part.
+# please note this is NOT rounding, it just removes the decimal part and keeps
+# the whole number part.
 # this will be covered later.
 
 # Did you know?

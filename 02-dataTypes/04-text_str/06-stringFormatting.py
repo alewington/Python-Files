6305fe63fb9b07484fd03d4b464afd247ec6cb66
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-05 20:27:40 +0100

    new file

--- 02-dataTypes/04-text_str/06-stringFormatting.py
+++ 02-dataTypes/04-text_str/06-stringFormatting.py
@@ -1,1 +1,11 @@
 # string Formatting
+
+# strings are immutable in Python.
+# Immutable means that once a string is created, it cannot be changed.
+# Any operation that seems to modify the string actually creates a new one.
+# So we are creating the string, destroying the string and creating a new one.
+
+# We can format strings in differents ways.
+# Basic formatting
+
+str_format = "text"

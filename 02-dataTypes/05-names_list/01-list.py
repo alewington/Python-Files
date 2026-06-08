b6dec116f2ee3231b143751dee8b3966d155a7a3
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-08 17:05:43 +0100

    new files

---
+++ 02-dataTypes/05-names_list/01-list.py
@@ -0,0 +1,21 @@
+# Lists
+
+# Lists are ordered collections of items. Items can be of different types.
+# For example, a list could contain strings, integers, and even other lists.
+names = ["Alice", "Bob"]
+print(names)
+
+# 1. Accessing List Elements
+print(names[0])
+
+# 2. Modifying List Elements
+names.append("David")
+print(names)
+
+# 3. Pop the last name from the list
+names.pop()
+print(names)
+
+# 4. Sort list descending alphabetically
+names.sort(reverse=True)
+print(names)

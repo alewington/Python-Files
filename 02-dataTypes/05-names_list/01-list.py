5c0c849a522df5648c151ca6697c52456a548056
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-12 09:28:09 +0100

    new files and updates

--- 02-dataTypes/05-names_list/01-list.py
+++ 02-dataTypes/05-names_list/01-list.py
@@ -2,6 +2,13 @@
 
 # Lists are ordered collections of items. Items can be of different types.
 # For example, a list could contain strings, integers, and even other lists.
+# it may look similar to arrays and can be used similarly to arrays
+# but Lists are not arrays.
+
+# More info about Lists vs. Arrays can be found here:
+# https://www.geeksforgeeks.org/python/difference-between-list-and-array-in-python/
+# I will cover arrays later in the lessons.
+
 names = ["Steve", "Alex"]
 print(names)
 
@@ -19,3 +26,17 @@ print(names)
 # 4. Sort list descending alphabetically
 names.sort(reverse=True)
 print(names)
+
+'''
+Basis	                List
+Data Type Handling	    Can store mixed data types
+Memory Usage	        Uses more memory due to flexible storage
+Performance	            Slower for numerical operations
+Flexibility	            Supports easy insertion, deletion and resizing
+Arithmetic Operations	Cannot perform element-wise arithmetic directly
+Built-in Support	    Available by default in Python
+Best Use Case	        General-purpose and mixed-type data
+Data Consistency	    Allows mixed values without restriction
+
+Compared to array module.
+'''
---
+++ 02-dataTypes/16-Arrays/00-arrays.py
@@ -0,0 +1,36 @@
+# Arrays
+
+# To use arrays properly in Python you need to use the ARRAY module.
+
+# Below information from
+# https://www.geeksforgeeks.org/python/difference-between-list-and-array-in-python/
+
+# Arrays
+
+# An array is a data structure that stores elements of the same data type in
+# contiguous (next to, near) memory locations, making it efficient for
+# numerical operations. Arrays require the array module to be used.
+
+# -Stores of the homogenous (same kind of) data only
+# -Uses less memory than lists
+# -Faster for numerical and mathematical operations
+# -Better suited for large numeric datasets
+
+import array as arr
+
+a = arr.array('i', [1, 2, 3])
+print(type(a))
+for i in a:
+    print(i, end=" ")
+
+"""
+Basis	                    Array
+Data Type Handling	        Stores only one data type
+Memory Usage	            Uses less memory because of uniform data type
+Performance	                Faster for numeric computations
+Flexibility	                Less flexible - fixed type and size constraints
+Arithmetic Operations	    Supports element-wise arithmetic
+Built-in Support	        Requires importing the array module
+Best Use Case	            Numeric and scientific data processing
+Data Consistency	        Enforces strict type consistency
+"""

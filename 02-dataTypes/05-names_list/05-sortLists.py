9d289d0e870552ab671f5a3926fdd95ec601b5f6
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-12 10:06:27 +0100

    new file

---
+++ 02-dataTypes/05-names_list/05-sortLists.py
@@ -0,0 +1,27 @@
+# Sorting Lists
+
+# Sorting lists are done by 2 methods:
+# 1. sort() - sorts the list in place
+# 2. sorted() - returns a new list that is sorted,
+# leaving the original list unchanged
+
+names = ["Alice", "Bob", "Charlie", "David", "Creeper", "Alex"]
+
+# Using sort()
+print("Original list:", names)
+
+# Using sorted()
+sorted_names = sorted(names)
+print("Original list after sorted() call:", names)
+# so no effect to orginal list is not sorted by sorted
+
+print("Newly sorted list from sorted():", sorted_names)
+# so new list is sorted by sorted
+
+names.sort()
+print("Sorted using sort():", names)
+# Shows original list is sorted by sort
+
+names.sort(reverse=True)
+print("Original list after sorted() call but reversed:", names)
+# Shows list is sorted by sort but now reversed (decending)

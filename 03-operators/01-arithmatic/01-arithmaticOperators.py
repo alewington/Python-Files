31feb3d3004929d8c2657330241d4445216caa12
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-26 09:37:05 +0100

    New File

---
+++ 03-operators/01-arithmatic/01-arithmaticOperators.py
@@ -0,0 +1,33 @@
+# Arithmatic Operators
+
+# The Arithmatic operators are  +, - , * , /, //, % and **
+
+# The + operator is used to add two numbers together.
+# + is also know as addition.
+print(10+2)
+
+# The - operator is used to subtract one number from another.
+# - is also known as subtraction
+print(10-2)
+
+# The * operator is used to multiply two numbers together.
+# * is also known as multiplication
+print(10*2)
+
+# The / operator is used to divide one number by another.
+# / is also known as float division.
+# The / means divide and give the answer as a float.
+print(10 / 2)
+
+# The % operator is used to find the remainder of a integer division problem.
+# % is also known as Modulus.
+# Modulus gives the remainder of the integer division. This means 10%3 gives 1.
+print(10 % 3)
+
+# integer division - this means 10//3 gives 3.
+# // is also known as floor division.
+print(10 // 3)
+
+# The ** operator is used to power two numbers together.
+# ** is also know as exponentiation.
+print(10 ** 2)

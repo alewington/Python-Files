504d5ee2f05115d303a73a2968e798a39c1b0f47
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-06-27 13:34:37 +0100

    New File Order Presedence

---
+++ 03-operators/01-arithmatic/00-BODMAS.py
@@ -0,0 +1,24 @@
+# Python Operator Precedence
+
+# BEDMAS, BODMAS, PEMDAS etc
+
+# Brackets, Operators, Division/Multiplication, Addition/Subtraction
+# are the order of operations. Highest at the top.
+
+# Python uses the following order of operations:
+#   Operator	            Description
+#   ()	                    Brackets
+#   **      	            Exponentiation
+#   +x  -x  ~x	            Unary plus, unary minus, and bitwise NOT
+#   *  /  //  %	            Multiplication, division, floor division & modulus
+#   +  -	                Addition and subtraction
+#   <<  >>	                Bitwise left and right shifts
+#   &	                    Bitwise AND
+#   ^	                    Bitwise XOR
+#   |	                    Bitwise OR
+# == !=  > # >=  <  <=      Comparisons
+# is  is not                Identity, non-identity
+# in  not in 	            Membership operators
+#   not	                    Logical NOT
+#   and	                    AND
+#   or	                    OR

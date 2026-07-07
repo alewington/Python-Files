614dfd85aba379556a04d6efd240ab39b0ad5056
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-07-07 10:02:14 +0100

    update

---
+++ 05-functions/01-functions/07-functionRecursion.py
@@ -0,0 +1,34 @@
+# Recursion
+
+# Recursion is a technique in which a function calls itself.
+# This can be used to solve problems that are naturally recursive, such as
+# traversing a tree or searching for an item in a list.
+
+# Advantages:
+# - Easy to understand
+# - Easy to write
+# - Easy to debug
+# - Can be used to solve problems that are naturally recursive
+# Disadvantages:
+# - Not as efficient as iterative solutions
+# - Can cause stack overflows if not written correctly
+# - Harder to read and maintain than iterative solutions
+# - more processor intensive
+
+# 1. Factorial
+def fibonacci(n):
+    """
+
+    Args:
+        n (_type_): _description_
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+
+for i in range(10):
+    print(fibonacci(i))

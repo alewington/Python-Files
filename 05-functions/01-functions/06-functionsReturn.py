74e1d3cef0a3074547d6f6c761a5c82d8da83d18
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-07-07 09:26:49 +0100

    update

--- 05-functions/01-functions/06-functionsReturn.py
+++ 05-functions/01-functions/06-functionsReturn.py
@@ -8,17 +8,44 @@ c: list[int] = []
 
 
 def choose() -> int:
+    """Chooses a number from user input. The user must enter an integer.
+        Args:
+            None
+        Returns:
+            int: The integer entered by the user
+        Example:
+            >>> choose()
+            5
+    """
     i: int = int(input("Choose a number to add:"))
     return i
 
 
 def numbers_to_add() -> list[int]:
+    """ Adds two numbers from user input of Choose()
+    Args:
+        None
+    Returns:
+        list[int]: The list of numbers entered by the user
+    Example:
+        >>> numbers_to_add()
+        [5, 7]
+    """
     for p in range(2):
         c.append(choose())
     return c
 
 
 def layout() -> str:
+    """ Adds two numbers from user input of Choose()
+    Args:
+        None
+    Returns:
+        str: _description_
+    Example:
+        >>> layout()
+        The sum of 5 and 7 is 12.
+    """
     numbers_to_add()
     return f"The sum of {c[0]} and {c[1]} is {sum(c)}."
 
